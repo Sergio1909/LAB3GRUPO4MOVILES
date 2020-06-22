db72a770e0379707a5def37a547d1714ebbbcfbc
Sergio Muro <sergio.muroc@pucp.pe>
2020-06-22 09:16:06 -0500

    Creacion de fragmentos locales

--- app/src/main/java/pucp/telecom/moviles/lab3/MainActivity.java
+++ app/src/main/java/pucp/telecom/moviles/lab3/MainActivity.java
@@ -1,8 +1,12 @@
 package pucp.telecom.moviles.lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
+
+import pucp.telecom.moviles.lab3.Fragments.DialogFragmentEjemplo;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void guardarLocalBtn(View view){
+        DialogFragmentEjemplo dialogFragmentEjemplo = new DialogFragmentEjemplo();
+        dialogFragmentEjemplo.show(getSupportFragmentManager(),"dialog fragment local");
+    }
+
+    public void guardarRemotoBtn(View view){
+        DialogFragmentEjemplo dialogFragmentEjemplo = new DialogFragmentEjemplo();
+        dialogFragmentEjemplo.show(getSupportFragmentManager(),"dialog fragment remoto");
+
+    }
+
 }
